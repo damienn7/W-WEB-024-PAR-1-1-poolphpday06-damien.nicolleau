699aace69ae73c90f05821458abac136b6fc4b7d
damienn7 <118727505+damienn7@users.noreply.github.com>
2022-11-29 10:23:31 +0100

    fonction cat_file_content testée et pushed

---
+++ ex_02.php
@@ -0,0 +1,22 @@
+<?php
+
+function my_cat_files(string $file_path)
+{   
+    $paths=func_get_args();
+    foreach($paths as $value)
+    {
+        $handle = fopen($value, "rb");
+        if($handle)
+        {
+        $content = fread($handle, filesize($value));
+        $contents.=$content."_____";
+        fclose($handle);
+        }
+    }
+    //echo $contents;
+    return $contents;
+}
+
+//my_cat_files("/Users/frimousse/W-WEB-024-PAR-1-1-poolphpday06-damien.nicolleau/test","/Users/frimousse/W-WEB-024-PAR-1-1-poolphpday06-damien.nicolleau/ex_01.php");
+
+
