b2f4f44dc96a71ac9fc846a3c3e78c34c9bede9a
damienn7 <118727505+damienn7@users.noreply.github.com>
2022-11-29 18:47:07 +0100

    Ajout de conditions dans la boucle foreach de l'exercice 2

--- ex_02.php
+++ ex_02.php
@@ -4,17 +4,29 @@ function my_cat_files(string $file_path)
 {   
     $contents="";
     $paths=func_get_args();
+    $num=func_num_args();
+    $i=0;
     foreach($paths as $value)
     {
         $handle = fopen($value, "rb");
         if($handle)
         {
         $content = fread($handle, filesize($value));
-        $contents.=$content."\n_____\n";
-        fclose($handle);
+            if($i>0 && $i<$num)
+            {
+                $contents.="\n_____\n".$content;
+                echo "2";
+            }
+            else
+            {
+                $contents.=$content;
+                echo "1";
+            }
+            fclose($handle);
         }
+        $i++;
     }
-    //echo $contents;
+    echo $contents;
     return $contents;
 }
 
