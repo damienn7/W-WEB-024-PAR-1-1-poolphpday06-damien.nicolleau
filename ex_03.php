4d9ca5cbd3a9978f97b037b487703a9489311266
damienn7 <118727505+damienn7@users.noreply.github.com>
2022-11-29 11:48:59 +0100

    test et push de l'exercice 3

---
+++ ex_03.php
@@ -0,0 +1,27 @@
+<?php
+
+function write_into_my_file(string $str, string $file, bool $append = false)
+{
+    if(is_writable($file))
+    {
+        if($append==true)
+        {
+            $handle=fopen($file,"a+");
+            $write=fwrite($handle,$str);
+            fclose($handle);
+        }
+        else{
+            $handle=fopen($file,"w+");
+            $write=fwrite($handle,$str);
+            fclose($handle);
+        }
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+    
+}
+
+//write_into_my_file("toto", "/Users/frimousse/W-WEB-024-PAR-1-1-poolphpday06-damien.nicolleau/test.txt",false);
